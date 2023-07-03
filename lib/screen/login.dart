c006a21251cc96e90a840f9746373b3902f73e80
vishnukhare <kharevishnu2@gmail.com>
2023-07-03 18:43:32 +0530

    day3 completed

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
+    return const Material(
       child: Center(
           child: Text(
         "LOGIN  kar lo koeee😫😫",
