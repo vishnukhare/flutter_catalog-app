a0332a0ae51594b410257bab9aae78352eeb93f3
vishnukhare <kharevishnu2@gmail.com>
2023-07-03 19:46:21 +0530

    chage

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_1/screen/login.dart';
 import 'screen/home.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,8 +15,8 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       themeMode: ThemeMode.light,
       theme: ThemeData(
-        primarySwatch: Colors.amber,
-      ),
+          primarySwatch: Colors.amber,
+          fontFamily: GoogleFonts.lato().fontFamily),
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -3,12 +3,26 @@ import 'package:flutter/material.dart';
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-          child: Text(
-        "LOGIN  kar lo koeee😫😫",
-        style: TextStyle(fontSize: 30),
-      )),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        children: [
+          Image.asset(
+            'assets/images/login_img.png',
+            fit: BoxFit.cover,
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Welcome To Login Page",
+            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold,),
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+        ],
+      ),
     );
   }
 }
